e103a44d7ede72f181aa0818a687ddb459cb72d9
Rikin Marfatia <rikinm10@gmail.com>
2015-10-25 22:17:28 -0400

    Added a very crude and basic color selection workflow, to set the background color of a task

---
+++ app/src/main/java/me/rikinmarfatia/mintask/util/ColorStrings.java
@@ -0,0 +1,11 @@
+package me.rikinmarfatia.mintask.util;
+
+/**
+ * Created by rikin on 10/25/15.
+ */
+public final class ColorStrings {
+    public static final String WHITE = "white";
+    public static final String RED = "red";
+    public static final String BLUE = "blue";
+    public static final String GREEN = "green";
+}
